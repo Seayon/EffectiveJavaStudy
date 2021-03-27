b8a6d8026c4af3ad896d38455298c3196f86de29
赵旭阳 <zhaoxuyang@wondersgroup.com>
2021-03-27 15:03:02 +0800

    add lirenhao

--- src/test/java/com/seayon/MainTest.java
+++ src/test/java/com/seayon/MainTest.java
@@ -4,8 +4,11 @@ package com.seayon;
 import com.seayon.chapter02.item6.Operation;
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.in;
 
 public class MainTest {
 
@@ -16,11 +19,11 @@ public class MainTest {
         assertThat(binaryA, equalTo(0B0100));
 
         int binaryB = binaryA & 0b1111;
-        assertThat(binaryB,equalTo(0b0100));
-        assertThat(binaryB,equalTo(4));
+        assertThat(binaryB, equalTo(0b0100));
+        assertThat(binaryB, equalTo(4));
 
-        assertThat(0b001|0b101,equalTo(0b101));
-        assertThat(0b001^0b101,equalTo(0b100));
+        assertThat(0b001 | 0b101, equalTo(0b101));
+        assertThat(0b001 ^ 0b101, equalTo(0b100));
 //        assertThat(~0b101,equalTo(0b010));
         System.out.println(Integer.toBinaryString(~0b101));
         System.out.println(Integer.toBinaryString(-6));
@@ -41,11 +44,78 @@ public class MainTest {
 
         String all = String.join("/", "1", "2", "3", "4");
         System.out.println("all = " + all);
-        
+
     }
-    
+
     @Test
-    public void test(){
-                
+    public void test() {
+
+        List<Set<Integer>> setList = new ArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            Set<Integer> integers = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                integers.add(j);
+            }
+            setList.add(integers);
+        }
+
+
+        List<Integer> integers1 = countList(setList);
+        System.out.println(integers1.toString());
+
+    }
+
+    public List<Integer> countList(List<Set<Integer>> setList) {
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> count = new HashMap<>();
+        for (Set<Integer> integers : setList) {
+            for (Integer integer : integers) {
+                if (count.containsKey(integer)) {
+                    Integer c = count.get(integer);
+                    c++;
+                    count.put(integer, c);
+                } else {
+                    count.put(integer, 1);
+                }
+            }
+        }
+        Set<Map.Entry<Integer, Integer>> entries = count.entrySet();
+        Iterator<Map.Entry<Integer, Integer>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> next = iterator.next();
+            Integer value = next.getValue();
+            if (value > 10) {
+                result.add(next.getKey());
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void testB() {
+        assertThat(addBefore(2), equalTo(1));
+        assertThat(addBefore(3), equalTo(1));
+        assertThat(addBefore(4), equalTo(2));
+        assertThat(addBefore(5), equalTo(4));
+        assertThat(addBefore(6), equalTo(7));
+        int i = addBefore(10000);
+        System.out.println(i);
+    }
+
+    /**
+     * 每个数等于前面三个数相加,初始的三个数已经给出,使用递归
+     * @param n
+     * @return
+     */
+    public int addBefore(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else {
+            return addBefore(n - 1) + addBefore(n - 2) + addBefore(n - 3);
+        }
     }
 }
