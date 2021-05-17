db4e40f430587e13c413821f950f12462acd3694
zhaoxuyang <zhaoxuyang@wondersgroup.com>
2021-05-17 14:23:47 +0800

    添加一些代码

--- src/main/java/com/seayon/jvm/GCWithAge15.java
+++ src/main/java/com/seayon/jvm/GCWithAge15.java
@@ -1,6 +1,15 @@
 package com.seayon.jvm;
 
-public class GCWithAge15 {
+import sun.java2d.opengl.CGLSurfaceData;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class GCWithAge15 implements Runnable{
     public static void main(String[] args) {
         byte[] arrayS = new byte[128 * 1024];
         byte[] array1 = new byte[2 * 1024 * 1024];
@@ -12,6 +21,36 @@ public class GCWithAge15 {
         }
 
         byte[] array4 = new byte[2 * 1024 * 1024];
+        Thread.currentThread().isInterrupted();
+        Thread.currentThread().setPriority(1);
+        int normPriority = Thread.NORM_PRIORITY;
+        Thread.currentThread().setDaemon(true);
+        GCWithAge15 gcWithAge15 = new GCWithAge15();
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+
+            }
+        });
+        Thread.currentThread().interrupt();
+        Thread.State state = Thread.currentThread().getState();
+        System.out.println(state);
+        ReentrantLock reentrantLock = new ReentrantLock();
+        // reentrantLock.tryLock(300, TimeUnit.DAYS);
+        // this.wait();
+        LinkedTransferQueue<Object> objects = new LinkedTransferQueue<>();
+        // objects.transfer("");
+        ConcurrentHashMap<Object, Object> objectObjectConcurrentHashMap = new ConcurrentHashMap<>(1, 0.71f, 2);
+        objectObjectConcurrentHashMap.size();
+
+        // AbstractQueuedLongSynchronizer.ConditionObject conditionObject = new AbstractQueuedLongSynchronizer.ConditionObject();
+
+
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
---
+++ src/main/java/com/seayon/jvm/HelloByteCode.java
@@ -0,0 +1,17 @@
+package com.seayon.jvm;
+
+/**
+ * @BelongProjecet EffectiveJavaStudy
+ * @BelongPackage com.seayon.jvm
+ * @Copyright 2018-2021 万达信息股份有限公司 - 医疗保障业务群
+ * @Author: 赵旭阳/Zhao Xuyang
+ * @Date: 2021/4/16 9:16 上午
+ * @Version V1.0
+ * @Description:
+ */
+
+public class HelloByteCode {
+    public static void main(String[] args) {
+        HelloByteCode helloByteCode = new HelloByteCode();
+    }
+}
