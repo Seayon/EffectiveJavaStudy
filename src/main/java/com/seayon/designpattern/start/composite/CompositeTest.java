fd0a30a78bae6f144bb58ddc8d6297a074ce533b
赵旭阳 <zhaoxuyang@wondersgroup.com>
2021-05-31 22:26:20 +0800

    添加组合模式

--- src/main/java/com/seayon/designpattern/start/composite/CompositeTest.java
+++ src/main/java/com/seayon/designpattern/start/composite/CompositeTest.java
@@ -3,6 +3,7 @@ package com.seayon.designpattern.start.composite;
 public class CompositeTest {
     public static void main(String[] args) {
 
+//        组合模式有点类似于,递归和二叉树,每个对象可以包含自身类型一样的对象,包含公共的方法,可以无限的任意的组合
         Component component1 = new Composite();
         Component component2 = new Composite();
 
